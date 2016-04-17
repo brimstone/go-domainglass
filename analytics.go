41cdc382986e718de1055618a7cd27d0af8f50fc
Matt Robinson <brimstone@the.narro.ws>
2016-04-17 09:40:37 -0400

    Fixing ClientRequest pk/unique relationship

--- analytics.go
+++ analytics.go
@@ -10,7 +10,7 @@ import (
 // ClientRequest TODO
 type ClientRequest struct {
 	Timestamp    time.Time `orm:"pk;column(timestamp);type(datetime)"`
-	IP           string    `orm:"pk;column(ip)"`
+	IP           string    `orm:"column(ip)"`
 	URL          string    `orm:"column(url)"`
 	UserAgent    string    `orm:"column(user-agent)"`
 	Referer      string
@@ -18,14 +18,12 @@ type ClientRequest struct {
 	ResponseTime time.Duration `orm:"column(reponse-time)"`
 }
 
-/*
 // TableUnique returns columns that should be unique
 func (c *ClientRequest) TableUnique() [][]string {
 	return [][]string{
-		[]string{"ip"},
+		{"timestamp", "ip"},
 	}
 }
-*/
 
 // Analytics TODO
 func Analytics(c *gin.Context) {
